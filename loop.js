f1323b2d5159650bad9fd14c6401754f500fbdba
ibrahim hossain <ibrahimhossaincse@gmail.com>
2021-04-04 22:06:08 -0700

    looping started and now for(){} loop

---
+++ loop.js
@@ -0,0 +1,75 @@
+/*loop:
+    All loops are doing samething.
+*/
+//add 1-100 all value using while loop
+num1 = 0;
+container = 0; //0 for reset memory location
+while(num1<8){
+    num1+=1;//break the loop when it's gone 50 times.
+    container+=num1;
+    sayHi();
+}
+//make a function then call it 100 times through while loop.
+console.log(container);
+function sayHi(){
+    console.log("I'm looping");
+}
+
+//Looking for an array element using while loop;
+var Name = ['jewel','hossain','kholil','mosto','abdur rabbi vai','others'];
+function nameList(arr,name){
+    var index = 0;
+    while(index<arr.length){
+        if(arr[index]===name){
+            console.log('Successed!');
+            console.log(arr[index] + ' '+ index);
+            break;
+        }else{
+            index++;
+        }
+    }
+}
+nameList(Name,'hossain');
+
+//do... while() loop here.
+var indexValue = 0;
+var shouldRunOnlyOnce = true;
+do{
+    indexValue++;
+    console.log("Your value is "+indexValue);
+    if(indexValue===10){
+        shouldRunOnlyOnce = false;
+    }
+}while(shouldRunOnlyOnce);
+
+//now make a condition value while press q then exit; do while loop execute the code minimum one time overhere.
+var condition = false;
+do{
+    var userInput = prompt("Enter your vlaue ");
+
+    if(userInput === 'q'){
+        condition = false;
+    }else{
+        condition = true;
+        console.log(userInput);
+    }
+}while(condition);
+
+//make a while loop for purchase an item.
+var purchase = true;
+var balance = 1000;
+do{
+    purchaseFunction(balance);
+    purchase = false;
+}while(balance>500);
+// Above 500 tk then you can purchase item.
+function purchaseFunction(itemValue) {
+    if(itemValue>500){
+        console.log("you can purchase the item");
+    }else{
+        console.log("you can not purchase the item");
+        break;
+    }
+}
+//Repeat until a specified conditions evaluates false.
+//Now start false value
